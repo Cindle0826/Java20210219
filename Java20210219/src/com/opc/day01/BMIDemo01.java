0714134945f9789fb77c37b5741fd6bc04f59334
cindle <cindle@localhost>
2021-02-22 18:50:06 +0800

    Project Demo

--- Java20210219/src/com/opc/day01/BMIDemo01.java
+++ Java20210219/src/com/opc/day01/BMIDemo01.java
@@ -1,7 +1,20 @@
 package com.opc.day01;
 
 public class BMIDemo01 {
-	public static void main(String[] args) {
-		System.out.println("Hello");
-	}
+    public static void main(String[] args) {
+        // 計算BMI
+        String name = "John";
+        double height = 170; // 170.0
+        double weight = 60; 
+        double result = weight / Math.pow(height/100, 2);
+        System.out.printf("%s 的身高: %.1f 體重: %.1f bmi: %.2f\n", 
+                name, height, weight, result);
+        
+        String name2 = "Mary";
+        double height2 = 160;
+        double weight2 = 48;
+        double result2 = weight2 / Math.pow(height2/100, 2);
+        System.out.printf("%s 的身高: %.1f 體重: %.1f bmi: %.2f\n", 
+                name2, height2, weight2, result2);
+    }
 }
---
+++ Java20210219/src/com/opc/day01/BMIDemo03.java
@@ -0,0 +1,9 @@
+package com.opc.day01;
+
+public class BMIDemo03 {
+    public static void main(String[] args) {
+        BMIDemo02.printBMI("John", 170, 60);
+        BMIDemo02.printBMI("Mary", 160, 48);
+        BMIDemo02.printBMI("Helen", 166, 65);
+    }
+}
